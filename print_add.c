79d3be679e6ac05d1ff11cfc2cd460d742ed0618
Freakywrites <Farouksanusi2000@gmail.com>
2022-10-18 12:25:16 -0700

    tired

--- print_add.c
+++ print_add.c
@@ -19,8 +19,8 @@ int print_add(va_list arguments, char *buf, unsigned int ibuf)
 	add = (va_arg(arguments, void *));
 	if (add == NULL)
 	{
-	for (i = 0; nill[i]; i++)
-	ibuf = handl_buf(buf, nill[i], ibuf);
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
 		return (5);
 	}
 	int_input = (intptr_t)add;
@@ -28,8 +28,8 @@ int print_add(va_list arguments, char *buf, unsigned int ibuf)
 	if (int_input < 0)
 	{
 		int_input = (int_input * -1) - 1;
-	isnegative = 1;
-		}
+		isnegative = 1;
+	}
 	binary = malloc(sizeof(char) * (64 + 1));
 	binary = fill_binary_array(binary, int_input, isnegative, 64);
 	hexadecimal = malloc(sizeof(char) * (16 + 1));
@@ -38,13 +38,14 @@ int print_add(va_list arguments, char *buf, unsigned int ibuf)
 	ibuf = handl_buf(buf, 'x', ibuf);
 	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
-	if (hexadecimal[i] != '0' && first_digit == 0)
-	first_digit = 1;
-	if (first_digit)
-	ibuf = handl_buf(buf, hexadecimal[i], ibuf);
+		if (hexadecimal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
 			count++;
-	}
 		}
+	}
 	free(binary);
 	free(hexadecimal);
 	return (count + 2);
--- print_num_hex.c
+++ print_num_hex.c
@@ -43,4 +43,5 @@ int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 	free(binary);
 	free(hexadecimal);
 	return (count + 2);
+}
 
